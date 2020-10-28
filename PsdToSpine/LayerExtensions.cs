086ff445c60016b8a83fd0300575bcabda1b2981
Newp Lee <mynewp@gmail.com>
2020-10-28 23:28:54 +0900

    BitMap으로 출력하는것 완성

--- PsdToSpine/LayerExtensions.cs
+++ PsdToSpine/LayerExtensions.cs
@@ -22,56 +22,33 @@ namespace PsdToSpine
 
             byte[] data = imageSource.MergeChannels();
             var channelCount = imageSource.Channels.Length;
-            var pitch = imageSource.Width * imageSource.Channels.Length;
+            var pitch = imageSource.Width * channelCount;
             var w = imageSource.Width;
             var h = imageSource.Height;
 
             //var format = channelCount == 3 ? TextureFormat.RGB24 : TextureFormat.ARGB32;
-            //var tex = new Texture2D(w, h, format, false);
-            var colors = new Color[data.Length / channelCount];
 
+            var bitmap = new Bitmap(w, h);
 
-            var k = 0;
-
-            var bitmap = new System.Drawing.Bitmap(w, h);
-            try
+            for (var y = h - 1; y >= 0; --y)
             {
-
-                for (var y = h - 1; y >= 0; --y)
+                for (var x = 0; x < pitch; x += channelCount)
                 {
-                    for (var x = 0; x < pitch; x += channelCount)
-                    {
-                        var n = x + y * pitch;
+                    var n = x + y * pitch;
+
+                    var b = data[n++];
+                    var g = data[n++];
+                    var r = data[n++];
+                    var a = channelCount == 4 ? (byte)System.Math.Round(data[n++] / 255f * imageSource.Opacity * 255f) : (byte)System.Math.Round(imageSource.Opacity * 255f);
 
 
-                        var b = data[n++];
-                        var g = data[n++];
-                        var r = data[n++];
-                        var a = channelCount == 4 ? (byte)System.Math.Round(data[n++] / 255f * imageSource.Opacity * 255f) : (byte)System.Math.Round(imageSource.Opacity * 255f);
+                    var color = Color.FromArgb(a, r, g, b);
 
-
-                        var color = Color.FromArgb(a, r, g, b);
-
-                        bitmap.SetPixel(x, y, color);
-                        Console.Write('.');
-                    }
+                    bitmap.SetPixel(x / channelCount, y, color);
                 }
-
             }
-            catch (Exception e)
-            {
-                Console.Write(e);
-
-            }
-
 
             return bitmap;
-
-            //BitmapSource
-
-            //if (channelCount == 4)
-            //    return BitmapSource.Create(imageSource.Width, imageSource.Height, 96, 96, PixelFormats.Bgra32, null, data, pitch);
-            //return BitmapSource.Create(imageSource.Width, imageSource.Height, 96, 96, PixelFormats.Bgr24, null, data, pitch);
         }
     }
 }
